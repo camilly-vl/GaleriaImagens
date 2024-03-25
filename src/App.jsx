20033de7213f962a353bc7ce2f7c91ee901f64a2
YuriMDP <yuriubatuba2019@gmail.com>
2024-03-24 22:18:30 -0300

    Não dá tempo

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import FotoAmpliada from './components/FotoAmpliada'
+import FotoList from './components/FotoList'
+import SearchBar from './components/SearchBar'
+
+import {useState, useEffect} from 'react'
+
+function App() {
+
+  return (
+    <div className='container'>
+      <SearchBar/>
+      <FotoList/>
+      <FotoAmpliada/>
+    </div>
+  )
+}
+
+export default App
